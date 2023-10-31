22b887e46b80bc6a355743bb66c54f4203f0d7b2
MarcusMThomsen <112625912+MarcusMThomsen@users.noreply.github.com>
2023-10-31 12:02:35 +0100

    Create get_pr_number.sh

---
+++ get_pr_number.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if no arguments were provided
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    echo "Usage: ./get_pr_number.sh <input_string>"
+    exit 1
+fi
+
+for var in "$@"
+do
+    if [[ $var =~ ^number:[0-9]+$ ]]; then
+        pr_number=${var#*:}
+        echo "pr_number is $pr_number"
+        echo "pr_number=$pr_number" >> $GITHUB_OUTPUT
+    fi
+done
